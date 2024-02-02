48980d410c0df0052d56e279193e9f2c56bbfa9c
halli21 <haraldurf21@ru.is>
2024-02-02 09:39:48 +0000

    base

--- Large assignment I - FritosJS/fritos.js
+++ Large assignment I - FritosJS/fritos.js
@@ -1,38 +1,18 @@
 
 
 
-class FritosElement {
-    constructor(nodes) {
-        this.nodes = nodes;
+
+class FritosObject {
+    constructor(items) {
+        this.items = items;
     }
-
-
-    setBackgroundColor(color) {
-        this.nodes.forEach(node => node.style.backgroundColor = color);
-        return this;
-    }
-
-    setColor(color) {
-        this.nodes.forEach(node => node.style.color = color);
-        return this;
-    }
-
- 
-
-    parent(selector) {
-        let parents = [];
-        this.nodes.forEach(node => {
-            if (node.parentElement && node.parentElement.matches(selector)) {
-                parents.push(node.parentElement);
-            }
-        });
-        return new FritosElement(parents);
-    }
-
-}
-
-function fritos(selector) {
-    return new FritosElement(document.querySelectorAll(selector));
 }
 
 
+
+
+
+window.fritos = function(selectors) {
+    const items = document.querySelectorAll(selectors);
+    return new FritosObject(items);
+};
