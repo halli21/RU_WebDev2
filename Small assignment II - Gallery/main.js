a3abfa4cde9fa5b49132dda3f600563d111411e1
halli21 <haraldurf21@ru.is>
2024-02-12 12:03:53 +0000

    semi setup

--- Small assignment II - Gallery/main.js
+++ Small assignment II - Gallery/main.js
@@ -1,24 +1,44 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.addEventListener('DOMContentLoaded', function() {
+  var dropzone = document.getElementById('dropzone');
+ 
+  
+  dropzone.addEventListener('dragenter', function(event) {
+      output.textContent = '';
+      event.stopPropagation();
+      event.preventDefault();
+  });
+  
+  dropzone.addEventListener('dragover', function(event) {
+      event.stopPropagation();
+      event.preventDefault();
+  });
+  
+  dropzone.addEventListener('drop', function(event) {
+      event.stopPropagation();
+      event.preventDefault();
+      dodrop(event);
+  });
+});
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
+
+function dodrop(event)
+{
+  var dt = event.dataTransfer;
+  var files = dt.files;
+
+  var count = files.length;
+  output("File Count: " + count + "\n");
+
+    for (var i = 0; i < files.length; i++) {
+      output(" File " + i + ":\n(" + (typeof files[i]) + ") : <" + files[i] + " > " +
+             files[i].name + " " + files[i].size + "\n");
+    }
+}
+
+function output(text)
+{
+  document.getElementById("dropzone").textContent += text;
+  //dump(text);
+}
