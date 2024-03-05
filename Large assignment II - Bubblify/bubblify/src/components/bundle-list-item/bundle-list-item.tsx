56cdedbd780fefe2e1b043553019ba51374284fc
halli21 <haraldurf21@ru.is>
2024-03-05 15:37:00 +0000

    jesus

--- Large assignment II - Bubblify/bubblify/src/components/bundle-list-item/bundle-list-item.tsx
+++ Large assignment II - Bubblify/bubblify/src/components/bundle-list-item/bundle-list-item.tsx
@@ -1,6 +1,5 @@
 import styles from "./bundle-list-item.module.css";
 import { Bundle } from "../../types/bundle";
-import { useNavigate } from "react-router-dom";
 import { useBundleBubbles } from "../../hooks/use-bundle-bubbles";
 
 
@@ -16,34 +15,42 @@ export const BundleListItem = ({ item } : BundleListItemProps) => {
         return total + (bubble?.price || 0);
     }, 0);
 
+    
+
 
     return (
-        <div>
-            <h2>{item?.name}</h2>
-            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex={-1} uk-slideshow="true">
+        <div className={styles.bundleListItem}>
 
-                <ul className="uk-slideshow-items">
+            <div className="uk-position-relative uk-visible-toggle uk-light" tabIndex={-1} uk-slideshow="autoplay: true, autoplay-interval: 3000" style={{height: "150px", width: "100%", aspectRatio: "1/1", display: "grid", justifyContent:"center"}}>
+                <ul className="uk-slideshow-items" style={{height: "150px", width: "150px"}}>
                     {bubblesList.map((bubble, index) => (
                         bubble && bubble.image ? (
                             <li key={index}>
-                                <img src={bubble.image} alt={bubble.name} uk-cover="true" />
+                                <img style={{height: "150px", width: "150px"}} src={bubble.image} alt={bubble.name} uk-cover="true" />
                             </li>
                         ) : null
                     ))}
                 </ul>
 
-                <a className="uk-position-center-left uk-position-small uk-hidden-hover" uk-slidenav-previous="true" uk-slideshow-item="previous"></a>
-                <a className="uk-position-center-right uk-position-small uk-hidden-hover" uk-slidenav-next="true" uk-slideshow-item="next"></a>
-
+                <a className="uk-position-center-left uk-position-small" uk-slidenav-previous="true" uk-slideshow-item="previous" style={{color: "gray"}}></a>
+                <a className="uk-position-center-right uk-position-small" uk-slidenav-next="true" uk-slideshow-item="next" style={{color: "gray"}}></a>
             </div>
 
-            <p>This bundle includes:</p>
-            {bubblesList.map((bubble) => (
-                <div>
-                    <p>{bubble?.name}</p>
-                </div>
-            ))}
-            <p>{bundlePrice} kr</p>
+
+            <div className={styles.heading}>
+                <h2>{item?.name}</h2>
+                <p>{bundlePrice} kr</p>
+            </div>
+                            
+            <div className={styles.includes}>
+                <p>This bundle includes:</p>
+                {bubblesList.map((bubble) => (
+                    <div>
+                        <p>{bubble?.name}</p> 
+                    </div>
+                ))}
+            </div>
+
            
         </div>
     );
