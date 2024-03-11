f04e718bfb9078d48cb796c54feac81bb389d932
halli21 <haraldurf21@ru.is>
2024-03-11 10:29:05 +0000

    about styles

--- Large assignment II - Bubblify/bubblify/src/pages/about/about.tsx
+++ Large assignment II - Bubblify/bubblify/src/pages/about/about.tsx
@@ -1,6 +1,15 @@
+import styles from "./about.module.css";
+
 export const About = () => (
-    <div>
-        <h1>About</h1>
-        <p>This is a description</p>
+    <div className={styles.about}>
+        <h1>About us</h1>
+        <p>
+            Bubblify is a pioneering company dedicated to creating innovative bubble solutions for kids. Established in 2023, our mission is to enhance playtime with high-quality, eco-friendly bubble products. Our flagship product, the EcoBubble Wand, is made from 100% recycled materials and produces vibrant, long-lasting bubbles. We are committed to sustainability, with a portion of our profits going towards environmental conservation efforts. With Bubblify, playtime becomes a magical, eco-conscious adventure.
+        </p>
+        <div>
+            <h3>Contact us</h3>
+            <p>Phone: +999 123 456</p>
+            <p>Email: inquiries@bubblify.com</p>
+        </div>
     </div>
 )
