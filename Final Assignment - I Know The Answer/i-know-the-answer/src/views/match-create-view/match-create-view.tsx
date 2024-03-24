09cc670071f6ce0e4b2fb35117596937f975ebfb
halli21 <haraldurf21@ru.is>
2024-03-24 21:29:25 +0000

    create match

--- Final Assignment - I Know The Answer/i-know-the-answer/src/views/match-create-view/match-create-view.tsx
+++ Final Assignment - I Know The Answer/i-know-the-answer/src/views/match-create-view/match-create-view.tsx
@@ -5,18 +5,67 @@ import {
     FormControl,
     Input,
     FormLabel,
-    Button
+    Button,
+    Radio
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Question } from "../../types/question";
+import { Option } from "../../types/option";
 
 
 export function MatchCreateView() {
     const [title, setTitle] = useState<string>("");
     const [titleImage, setTitleImage] = useState<string>("https://lumiere-a.akamaihd.net/v1/images/image_3e7881c8.jpeg?region=131,0,1338,753");
-    const [questions, setQuestions] = useState<Question[]>([]);
+    
+    const initialAnswer: Option = {
+        value: "",
+        correct: false,
+    };
+      
+    const initialQuestion: Question = {
+        title: "",
+        options: new Array(4).fill(null).map(() => ({ ...initialAnswer })),
+    };
+
+    const [questions, setQuestions] = useState<Question[]>([initialQuestion]);
+
+
+    const updateQuestionTitle = (index: number, newTitle: string) => {
+        const updatedQuestions = [...questions];
+        updatedQuestions[index].title = newTitle;
+        setQuestions(updatedQuestions);
+    };
+    
+    const updateOptionValue = (questionIndex: number, optionIndex: number, newValue: string) => {
+        const updatedQuestions = [...questions];
+        updatedQuestions[questionIndex].options[optionIndex].value = newValue;
+        setQuestions(updatedQuestions);
+    };
+    
+      
+    const setCorrectOption = (questionIndex: number, optionIndex: number) => {
+        const updatedQuestions = [...questions];
+    
+        updatedQuestions[questionIndex].options.forEach(answer => {
+            answer.correct = false;
+        });
+  
+        updatedQuestions[questionIndex].options[optionIndex].correct = true;
+        setQuestions(updatedQuestions);
+    };
+
+    const addNewQuestion = () => {
+        setQuestions(prevQuestions => [
+            ...prevQuestions,
+            {...initialQuestion}
+        ]);
+    };
+
+    const removeQuestion = (questionIndex: number) => {
+        setQuestions(prevQuestions => prevQuestions.filter((questoin, index) => index !== questionIndex));
+    };
 
     const navigate = useNavigate();
 
@@ -42,8 +91,49 @@ export function MatchCreateView() {
                         value={titleImage} 
                         onChange={(evt) => setTitleImage(evt.target.value)}/>
                 </FormControl>
-                <Button>Save</Button>
             </Stack>
+
+
+            <Heading as='h3' size='lg' style={{paddingTop: "100px"}}>Questions</Heading>
+            <Button onClick={addNewQuestion}>Add New Question</Button>
+            {questions.map((question, questionIndex) => (
+                <Stack key={questionIndex} spacing={2}>
+                    <Heading as='h5' size='sm' style={{paddingTop: "50px"}}>{`Question ${questionIndex + 1}`}</Heading>
+                    <FormControl>
+                        <FormLabel>Title of the question</FormLabel>
+                        <Input 
+                        type="text"
+                        placeholder="Enter the title of the question"
+                        value={question.title}
+                        onChange={(e) => updateQuestionTitle(questionIndex, e.target.value)}
+                        />
+                    </FormControl>
+
+                    {question.options.map((option, optionIndex) => (
+                        <FormControl key={optionIndex}>
+                        <FormLabel>{`Answer ${optionIndex + 1}`}</FormLabel>
+                        <Input 
+                            type="text"
+                            placeholder="Enter Answer"
+                            value={option.value}
+                            onChange={(e) => updateOptionValue(questionIndex, optionIndex, e.target.value)}
+                        />
+                        <Radio
+                            isChecked={option.correct}
+                            onChange={() => setCorrectOption(questionIndex, optionIndex)}
+                        >
+                            Right answer?
+                        </Radio>
+                        </FormControl>
+                    ))}
+
+                    <Button onClick={() => removeQuestion(questionIndex)}>Remove</Button>
+          
+                </Stack>
+            ))}
+
+
+            <Button>Save</Button>
         </Box>
     )
 }
